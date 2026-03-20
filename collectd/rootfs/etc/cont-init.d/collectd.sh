22de13ed3f6fdfff7f67df95c96fa55416cf045f
Nicolas Cocu <nicolas@home.noisyspoon.net>
2026-03-20 13:59:49 +0100

    setup

--- collectd/rootfs/etc/cont-init.d/collectd.sh
+++ collectd/rootfs/etc/cont-init.d/collectd.sh
@@ -7,15 +7,15 @@
 HA_HOSTNAME=$(bashio::config 'hostname')
 GRAPHITE_HOST=$(bashio::config 'graphite_host')
 GRAPHITE_PREFIX=$(bashio::config 'graphite_prefix')
-GRAPHITE_EXPORTER_HOST=$(bashio::config 'graphite-exporter_host')
-GRAPHITE_EXPORTER_PORT=$(bashio::config 'graphite-exporter_port')
+GRAPHITE_EXPORTER_HOST=$(bashio::config 'graphite_exporter_host')
+GRAPHITE_EXPORTER_PORT=$(bashio::config 'graphite_exporter_port')
 
 sed -i \
     -e "s/{{hostname}}/${HA_HOSTNAME}/g" \
     -e "s/{{graphite_host}}/${GRAPHITE_HOST}/g" \
     -e "s/{{graphite_prefix}}/${GRAPHITE_PREFIX}/g" \
-    -e "s/{{graphite-exporter_host}}/${GRAPHITE_EXPORTER_HOST}/g" \
-    -e "s/{{graphite-exporter_port}}/${GRAPHITE_EXPORTER_PORT}/g" \
+    -e "s/{{graphite_exporter_host}}/${GRAPHITE_EXPORTER_HOST}/g" \
+    -e "s/{{graphite_exporter_port}}/${GRAPHITE_EXPORTER_PORT}/g" \
     /etc/collectd/collectd.conf
 
 bashio::log.info "Service setup applied"
