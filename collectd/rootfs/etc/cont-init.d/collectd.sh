3549ce17bfec6c618e4ee98a40d688f99ac49fe5
Nicolas Cocu <nicolas@noisyspoon.net>
2026-03-30 09:12:33 +0200

    dsiabling

--- collectd/rootfs/etc/cont-init.d/collectd.sh
+++ collectd/rootfs/etc/cont-init.d/collectd.sh
@@ -6,11 +6,12 @@
 
 HA_HOSTNAME=$(bashio::config 'hostname')
 ENABLE_GRAPHITE_PLUGIN=$(bashio::config 'enable_graphite_plugin')
-ENABLE_WRITE_PROMETHEUS_PLUGIN=$(bashio::config 'enable_write_prometheus_plugin')
 GRAPHITE_HOST=$(bashio::config 'graphite_host')
 GRAPHITE_PREFIX=$(bashio::config 'graphite_prefix')
 GRAPHITE_EXPORTER_HOST=$(bashio::config 'graphite_exporter_host')
 GRAPHITE_EXPORTER_PORT=$(bashio::config 'graphite_exporter_port')
+ENABLE_WRITE_PROMETHEUS_PLUGIN=$(bashio::config 'enable_write_prometheus_plugin')
+PROMETHEUS_PORT=$(bashio::config 'prometheus_port')
 
 sed -i \
     -e "s/{{hostname}}/${HA_HOSTNAME}/g" \
@@ -18,16 +19,23 @@ sed -i \
     -e "s/{{graphite_prefix}}/${GRAPHITE_PREFIX}/g" \
     -e "s/{{graphite_exporter_host}}/${GRAPHITE_EXPORTER_HOST}/g" \
     -e "s/{{graphite_exporter_port}}/${GRAPHITE_EXPORTER_PORT}/g" \
+    -e "s/{{prometheus_port}}/${PROMETHEUS_PORT}/g" \
     /etc/collectd/collectd.conf
 
 if [ "${ENABLE_GRAPHITE_PLUGIN}" == "true" ]; then
     bashio::log.info "Enabling Graphite plugin"
-    sed -i "s/#LoadPlugin graphite/LoadPlugin graphite/g" /etc/collectd/collectd.conf
+    sed -i "s/^#LoadPlugin graphite/LoadPlugin graphite/g" /etc/collectd/collectd.conf
+else
+    bashio::log.info "Disabling Graphite plugin"
+    sed -i "s/^LoadPlugin graphite/#LoadPlugin graphite/g" /etc/collectd/collectd.conf
 fi
 
 if [ "${ENABLE_WRITE_PROMETHEUS_PLUGIN}" == "true" ]; then
     bashio::log.info "Enabling Prometheus plugin"
-    sed -i "s/#LoadPlugin write_prometheus/LoadPlugin write_prometheus/g" /etc/collectd/collectd.conf
+    sed -i "s/^#LoadPlugin write_prometheus/LoadPlugin write_prometheus/g" /etc/collectd/collectd.conf
+else
+    bashio::log.info "Disabling Prometheus plugin"
+    sed -i "s/^LoadPlugin write_prometheus/#LoadPlugin write_prometheus/g" /etc/collectd/collectd.conf
 fi
 
 bashio::log.info "Service setup applied"
