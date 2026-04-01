cd8712659792412331183855d20140210b5ad2e8
Nicolas Cocu <nicolas@home.noisyspoon.net>
2026-04-01 17:37:20 +0200

    var

--- collectd/rootfs/opt/bin/mqtt_sensors.sh
+++ collectd/rootfs/opt/bin/mqtt_sensors.sh
@@ -25,10 +25,10 @@ for metric in $metrics
 do
     node_name=`echo $metric | cut -d'/' -f3`
     metric_name=`echo $metric | cut -d'/' -f4`
-    if [[ $node_name =~ "cpu-" ]]
+    if [[ $node_name =~ "^cpu-[0-9]$" ]]
     then      
         bashio::log.info "Processing metric: $metric ($node_name/$metric_name)"
-        data='{"name": "${node_name} ${metric_name}", "state_topic": "$metric", "unique_id": "${node_name}_${metric_name}", "value_template": "{{ (value.split(':')[1].split('\0')[0] | float(0)) | round(1) }}", "state_class": "measurement", "icon": "mdi:cpu-64-bit", "device": $device}'
+        data="{\"name\": \"${node_name} ${metric_name}\", \"state_topic\": \"$metric\", \"unique_id\": \"${node_name}_${metric_name}\", \"value_template\": \"{{ (value.split(':')[1].split('\\0')[0] | float(0)) | round(1) }}\", \"state_class\": \"measurement\", \"icon\": \"mdi:cpu-64-bit\", \"device\": $device}"
         bashio::log.info "mosquitto_pub -r -h core-mosquitto -p 1883 -u $MQTT_USER -P $MQTT_PASSWORD -t $metric -m '$data'"
         mosquitto_pub -r -h core-mosquitto -p 1883 -u $MQTT_USER -P $MQTT_PASSWORD -t $metric -m '$data'
     fi
